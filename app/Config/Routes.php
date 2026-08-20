7cb23acec5355c17c9e71c12789d0be7601a4578
Ardhea Regita Ramadhani <ardhearamadhani@gmail.com>
2026-08-20 10:06:46 +0700

    Menyesuaikan routing dan controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -4,7 +4,4 @@ use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
 $routes->get('/', 'Home::index');
-$routes->get('profil', 'Profil::index');
-$routes->get('jurusan', 'Jurusan::index');
-$routes->get('fasilitas', 'Fasilitas::index');
-$routes->get('kontak', 'Kontak::index');
+
